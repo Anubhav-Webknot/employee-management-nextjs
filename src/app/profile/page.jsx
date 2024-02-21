bdcd2d987217876241a31fbd2777150b580c28d2
Anubhav Nath <anubhav.n@webknot.in>
2024-02-21 17:08:52 +0530

    navbar and profile bug fixes

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -40,7 +40,7 @@ export default function profile(){
         </li>
     </ul>
     <div class="p-4 border-t mx-8 mt-2">
-        <button class="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Message</button>
+        <button class="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Submit</button>
     </div>
 </div>
         </main>
