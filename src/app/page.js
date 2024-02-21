90bae9056a7950f91594626ce6cddf85f2c27d37
Anubhav Nath <anubhav.n@webknot.in>
2024-02-21 12:18:07 +0530

    updated email validation

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,6 @@ export default function Home() {
           <label for="email"></label>
             <input
               type="email"
-              
               name="email"
               id="email"
               className="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
